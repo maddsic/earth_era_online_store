1da4a6b6263d82894693f04059e82e80c9b6d2e6
maddsic <sainsidibeh@yahoo.com>
2025-05-21 21:45:26 -0400

    Remove sanity-studio folder from Git tracking

--- sanity-studio/studio/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import {heroSlides} from './hero'
-import {CategoryType} from './categorySchema'
-import {productType} from './productsSchema'
-
-export const schemaTypes = [heroSlides, CategoryType, productType]
